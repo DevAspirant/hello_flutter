f87cb3409262367bd4799f33acfec3e1b7ce04d4
ammar falmban <devAspirant@gmail.com>
2018-12-13 18:23:23 +0300

    publish

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './UI/home.dart';
+
+void main(){runApp(
+  new MaterialApp(
+    title: "Welcome App",
+    home: new Welcome(),
+  ));
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(
+    new Material(
+      color: Colors.deepOrange,
+      child: new Center(
+        child: new Text(
+          "Hello into VCode",
+        ),
+      )
+  )
+  );
+}
